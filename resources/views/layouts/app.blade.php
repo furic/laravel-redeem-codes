b5f903b1e3b38898444361310df12624fee777a3
Richard Fu <fur@richardfu.net>
2020-12-05 17:32:19 +1100

    Rename title in layout blade.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	    <meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Sweaty Chair Studio</title>
+        <title>Redeem Codes - Furic</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 	    <!-- Styles -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
